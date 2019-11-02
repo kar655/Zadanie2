7e4f1266c450bbf7bd6de9afc242696f4361cc1f
kar655 <karol123zag@gmail.com>
2019-11-02 09:52:57 +0100

    Works

--- leftist.ml
+++ leftist.ml
@@ -1,33 +1,54 @@
 (** Typ złączalnej kolejki priorytetowej *)
 type 'a queue =
-| Node of 'a * 'a queue * 'a queue
-| Null
+| Node of 'a * int * 'a queue * 'a queue
+| Null of int
 
-let a = Node(5, Node(10, Null, Null), Node(12, Null, Null))
-let b = Node(3, Node(7, Node(14, Null, Null), Null), Node(8, Null, Null))
+(** Pusta kolejka priorytetowa *)
+let (empty: 'a queue) = Null(-1)
+
+let a = Node(5,1,Node(10,0,empty,empty),Node(12,0,empty,empty))
+let b = Node(3, 1, Node(7,0,Node(14,0,empty,empty),empty), Node(8,0,empty,empty))
 
 (** [join q1 q2] zwraca złączenie kolejek [q1] i [q2] *)
 exception Blad
 
+
 let rec join (q1: 'a queue) (q2: 'a queue) =
   match q1, q2 with
-  | Null,Null -> Null
-  | q1, Null -> q1
-  | Null, q2 -> q2
-  | Node(value1, llh1, rlh1), Node(value2, llh2, rlh2) ->
-    if value1 < value2 then
-      Node(value1, llh1, join rlh1 q2 )
-    else if value1 > value2 then
-      Node(value2, llh2, join rlh2 q1)
-    else raise (Blad)
+  | Null(_), Null(_) -> empty
+  | q1, Null(_) -> q1
+  | Null(_), q2 -> q2
+  | Node(value1, rheight1, l1, r1), Node(value2, rheight2, l2, r2) ->
+    if value1 <= value2 then   (* Node(value1, l1, join rlh1 q2 ) *)
+      (* if npl(h1)  *)
+      let Node(_, rheight3, _, _) as q3 = join r1 q2 in
 
-(** Pusta kolejka priorytetowa *)
-let (empty: 'a queue) = Null
+      if l1 = empty then Node(value1, rheight3, q3, empty)
+      else
+
+        let Node(_, rheightl1, _, _) = l1 in
+
+        if rheightl1 <= rheight3 then
+          Node(value1, rheightl1, q3, l1)
+        else
+          Node(value1, rheight3, l1, q3)
+
+    else
+    
+      let Node(_, rheight3, _, _) as q3 = join r2 q1 in
+      if l2 = empty then Node(value2, rheight3, q3, empty)
+      else
+        let Node(_, rheightl2, _, _) = l2 in
+
+        if rheightl2 <= rheight3 then
+          Node(value2, rheightl2, q3, l2)
+        else
+          Node(value2, rheight3, l2, q3)
 
 (** [add e q] zwraca kolejkę powstałą z dołączenia elementu [e]
     do kolejki [q] *)
 let add e (q: 'a queue) =
-  join (Node(e, Null, Null)) q
+  join (Node(e, 0, empty, empty)) q
 
 (** Wyjątek podnoszony przez [delete_min] gdy kolejka jest pusta *)
 exception Empty
@@ -37,11 +58,11 @@ exception Empty
     Jeśli [q] jest puste podnosi wyjątek [Empty]. *)
 let delete_min (q: 'a queue) =
   match q with
-  | Null -> raise (Empty)
-  | Node(x, l, r) -> (x, join l r)
+  | Null(_) -> raise (Empty)
+  | Node(x, _, l, r) -> (x, join l r)
 
 
 
 (** Zwraca [true] jeśli dana kolejka jest pusta. W przeciwnym razie [false] *)
 let is_empty (q: 'a queue) =
-  if q = Null then true else false
+  if q = empty then true else false
